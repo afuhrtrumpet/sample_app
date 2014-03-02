5867928831691e7e5cbc057537886067af3ed34f
Alex Fuhr <afuhrtrumpet>
2014-03-02 10:28:32 -0500

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
 	factory :user do
-		name "Alex Fuhr"
-		email "fuhr.8@osu.edu"
+		sequence(:name) { |n| "Person #{n}" }
+		sequence(:email) { |n| "person_#{n}@example.com" }
 		password "foobar"
 		password_confirmation "foobar"
+
+		factory :admin do
+			admin true
+		end
 	end
 end
